b81662e61c64dd6eb7b3334ebbeda844190049e5
Curtis-96 <curtisfraserteitleman@gmail.com>
2020-06-25 22:37:20 -0400

    Add files via upload
    style_json.json file establishes maps styling, all files in commit are for seperate android project that implementes google maps

---
+++ MapsMarkerActivity.java
@@ -0,0 +1,104 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.example.mapwithmarker;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * An activity that displays a Google map with a marker (pin) to indicate a particular location.
+ */
+// [START maps_marker_on_map_ready]
+public class MapsMarkerActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+    private static final String TAG = MapsMarkerActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
+        setContentView(R.layout.activity_maps);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    // [END maps_marker_get_map_async]
+    // [END_EXCLUDE]
+
+    // [START_EXCLUDE silent]
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user receives a prompt to install
+     * Play services inside the SupportMapFragment. The API invokes this method after the user has
+     * installed Google Play services and returned to the app.
+     */
+    // [END_EXCLUDE]
+    // [START maps_marker_on_map_ready_add_marker]
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // [START_EXCLUDE silent]
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        // [END_EXCLUDE]
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+
+        LatLng boston = new LatLng(42.3601, -71.0589);
+        LatLng wit = new LatLng(42.336297, -71.095531);
+        LatLng mfa = new LatLng(42.3376018, -71.1907905);
+
+
+        googleMap.addMarker(new MarkerOptions()
+            .position(wit)
+            .title("This is wentworth"));
+        googleMap.addMarker(new MarkerOptions().position(mfa).title("Museum of fine arts"));
+        googleMap.addMarker(new MarkerOptions().position(mfa).title("This is boston"));
+        googleMap.addMarker(new MarkerOptions().position(wit).title("wentworth"));
+        // [START_EXCLUDE silent]
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(wit));
+        googleMap.setMinZoomPreference(15.0f);
+        //googleMap.setMaxZoomPreference(20.0f);
+        // [END_EXCLUDE]
+    }
+    // [END maps_marker_on_map_ready_add_marker]
+}
+// [END maps_marker_on_map_ready]
